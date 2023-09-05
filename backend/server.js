d364866805fcbd8044fef0c1b603e21ffac54e22
chaiwat poondee <cwpd@CWPD.local>
2023-09-06 00:39:34 +0700

    backend user login update

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,43 @@
-var http = require('http');
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
 
-http.createServer(function (req, res) {
- res.writeHead(200, {'Content-Type': 'text/plain'});
- res.end('Hello World');
-}).listen(8081, '127.0.0.1');
+const app = express();
 
+const corsOptions = {
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  };
 
-console.log('Server running at http://127.0.0.1:8081/');
+app.use(cors(corsOptions));
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "25452545",
+  database: "Technopolis",
+});
+
+con.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL database:", err);
+  } else {
+    console.log("Connected to MySQL database!");
+  }
+});
+
+app.get("/api/user", (req, res) => {
+  con.query("SELECT * FROM user", (err, result) => {
+    if (err) {
+      console.error("เกิดข้อผิดพลาด", err);
+      res.status(500).json({ err: "เกิดข้อผิดพลาด" });
+    } else {
+      res.setHeader("Content-Type", "application/json");
+      res.send(JSON.stringify(result, null, 2));
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log(`Server is running`);
+});
