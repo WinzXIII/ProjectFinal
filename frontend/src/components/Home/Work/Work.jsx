e2463a915005912b475a5521989544e780b91cae
chaiwat poondee <cwpd@CWPD.local>
2023-09-05 18:33:39 +0700

    update work page

--- frontend/src/components/Home/Work/Work.jsx
+++ frontend/src/components/Home/Work/Work.jsx
@@ -66,26 +66,39 @@ const Work = () => {
             <h1>ภาระงานหลักและผลการปฎิบัติงาน</h1>
             <div className="work-main-box">
               <div className="work-box-nav">
-                <div className="work-nav-number">ลำดับ</div>
-                <div className="work-nav-work">ภาระงาน</div>
-                <div className="work-nav-detail">รายละเอียด</div>
-                <div className="work-nav-result">ผลการปฏิบัติงาน</div>
+                <div className="work-main-number">ลำดับ</div>
+                <div className="work-main-work">ภาระงาน</div>
+                <div className="work-main-detail">รายละเอียด</div>
+                <div className="work-main-result">ผลการปฏิบัติงาน</div>
+                <div className="work-main-remove"></div>
               </div>
               {Array.from({ length: count }, (_, index) => (
                 <div className="work-box-input" key={index}>
-                  <div className="work-input-number">{index + 1}</div>
-                  <div className="work-input-work">
-                    <input type="text" />
+                  <div className="work-main-number">{index + 1}</div>
+                  <div className="work-main-work">
+                    <input type="text" required />
                   </div>
-                  <div className="work-input-detail">
-                    <input type="text" />
+                  <div className="work-main-detail">
+                    <input type="text" required />
                   </div>
-                  <div className="work-input-result">
-                    <select id={`select${index}`}>
-                      <option value="none"></option>
+                  <div className="work-main-result">
+                    <select id={`select${index}`} required>
+                      <option value=""></option>
                       <option value="success">เสร็จสิ้น</option>
                       <option value="wait">อยู่ระหว่างดำเนินการ</option>
                     </select>
+                  </div>
+                  <div className="work-main-remove">
+                    <button
+                      type="button"
+                      onClick={() => {
+                        if (count > 1) {
+                          setCount(count - 1);
+                        }
+                      }}
+                    >
+                      ลบ
+                    </button>
                   </div>
                 </div>
               ))}
@@ -99,25 +112,190 @@ const Work = () => {
           </div>
 
           <div className="work-form-other">
-            <h1>งานอื่น/งานพิเศษและผลการปฏิบัติงาน</h1>
-            <div className="work-other-box"></div>
+            <h1>งานอื่น / งานพิเศษและผลการปฏิบัติงาน</h1>
+            <div className="work-other-box">
+              <div className="work-box-nav">
+                <div className="work-other-number">ลำดับ</div>
+                <div className="work-other-work">งานอื่นๆ/งานพิเศษ</div>
+                <div className="work-other-result">ผลการปฏิบัติงาน</div>
+                <div className="work-other-remove"></div>
+              </div>
+              {Array.from({ length: count }, (_, index) => (
+                <div className="work-box-input">
+                  <div className="work-other-number">{index + 1}</div>
+                  <div className="work-other-work">
+                    <input type="text" required />
+                  </div>
+                  <div className="work-other-result">
+                    <select id={`select${index}`} required>
+                      <option value=""></option>
+                      <option value="success">เสร็จสิ้น</option>
+                      <option value="wait">อยู่ระหว่างดำเนินการ</option>
+                    </select>
+                  </div>
+                  <div className="work-other-remove">
+                    <button
+                      type="button"
+                      onClick={() => {
+                        if (count > 1) {
+                          setCount(count - 1);
+                        }
+                      }}
+                    >
+                      ลบ
+                    </button>
+                  </div>
+                </div>
+              ))}
+              <div className="work-box-add">
+                <button type="button" onClick={() => setCount(count + 1)}>
+                  <IoMdAddCircleOutline size="20px" />
+                  เพิ่มข้อมูล
+                </button>
+              </div>
+            </div>
           </div>
 
           <div className="work-form-creative">
             <h1>
-              นวัตกรรม/งานที่เกิดจากความคิดสร้างสรรค์/การพัฒนางานและผลที่ได้รับ
+              นวัตกรรม / งานที่เกิดจากความคิดสร้างสรรค์ /
+              การพัฒนางานและผลที่ได้รับ
             </h1>
-            <div className="work-creative-box"></div>
+            <div className="work-creative-box">
+              <div className="work-box-nav">
+                <div className="work-creative-number">ลำดับ</div>
+                <div className="work-creative-work">งานอื่นๆ/งานพิเศษ</div>
+                <div className="work-creative-result">ผลการปฏิบัติงาน</div>
+                <div className="work-creative-remove"></div>
+              </div>
+              {Array.from({ length: count }, (_, index) => (
+                <div className="work-box-input">
+                  <div className="work-creative-number">{index + 1}</div>
+                  <div className="work-creative-work">
+                    <input type="text" required />
+                  </div>
+                  <div className="work-reative-result">
+                    <select id={`select${index}`} required>
+                      <option value=""></option>
+                      <option value="success">เสร็จสิ้น</option>
+                      <option value="wait">อยู่ระหว่างดำเนินการ</option>
+                    </select>
+                  </div>
+                  <div className="work-creative-remove">
+                    <button
+                      type="button"
+                      onClick={() => {
+                        if (count > 1) {
+                          setCount(count - 1);
+                        }
+                      }}
+                    >
+                      ลบ
+                    </button>
+                  </div>
+                </div>
+              ))}
+              <div className="work-box-add">
+                <button type="button" onClick={() => setCount(count + 1)}>
+                  <IoMdAddCircleOutline size="20px" />
+                  เพิ่มข้อมูล
+                </button>
+              </div>
+            </div>
           </div>
 
           <div className="work-form-university">
             <h1>หน่วยงานอื่นที่ติดต่อประสานงานทั้งภายในและภายนอกมหาวิทยาลัย</h1>
-            <div className="work-university-box"></div>
+            <div className="work-university-box">
+              <div className="work-box-nav">
+                <div className="work-university-type">ประเภทหน่วยงาน</div>
+                <div className="work-university-agency">ชื่อหน่วยองค์กร</div>
+                <div className="work-university-remove"></div>
+              </div>
+              {Array.from({ length: count }, (_, index) => (
+                <div className="work-box-input">
+                  <div className="work-university-type">
+                    <select id={`select${index}`} required>
+                      <option value=""></option>
+                      <option value="success">ภายใน</option>
+                      <option value="wait">ภายนอก</option>
+                    </select>
+                  </div>
+                  <div className="work-university-agency">
+                    <input type="text" required />
+                  </div>
+                  <div className="work-university-remove">
+                    <button
+                      type="button"
+                      onClick={() => {
+                        if (count > 1) {
+                          setCount(count - 1);
+                        }
+                      }}
+                    >
+                      ลบ
+                    </button>
+                  </div>
+                </div>
+              ))}
+              <div className="work-box-add">
+                <button type="button" onClick={() => setCount(count + 1)}>
+                  <IoMdAddCircleOutline size="20px" />
+                  เพิ่มข้อมูล
+                </button>
+              </div>
+            </div>
           </div>
 
           <div className="work-form-reward">
             <h1>รางวัลที่ได้รับจากการปฏิบัติงาน</h1>
-            <div className="work-reward-box"></div>
+            <div className="work-reward-box">
+              <div className="work-box-nav">
+                <div className="work-reward-number">ลำดับ</div>
+                <div className="work-reward-name">ชื่อรางวัล</div>
+                <div className="work-reward-type">ลักษณะรางวัลที่ได้รับ</div>
+                <div className="work-reward-agency">
+                  หน่วยงานที่ได้รับมอบหมาย
+                </div>
+                <div className="work-reward-date">วันที่ได้รับรางวัล</div>
+                <div className="work-reward-remove"></div>
+              </div>
+              {Array.from({ length: count }, (_, index) => (
+                <div className="work-box-input">
+                  <div className="work-reward-number">{index + 1}</div>
+                  <div className="work-reward-name">
+                    <input type="text" required />
+                  </div>
+                  <div className="work-reward-type">
+                    <input type="text" required />
+                  </div>
+                  <div className="work-reward-agency">
+                    <input type="text" required />
+                  </div>
+                  <div className="work-reward-date">
+                    <input className="work-reward-date-settings" type="date" required />
+                  </div>
+                  <div className="work-reward-remove">
+                    <button
+                      type="button"
+                      onClick={() => {
+                        if (count > 1) {
+                          setCount(count - 1);
+                        }
+                      }}
+                    >
+                      ลบ
+                    </button>
+                  </div>
+                </div>
+              ))}
+              <div className="work-box-add">
+                <button type="button" onClick={() => setCount(count + 1)}>
+                  <IoMdAddCircleOutline size="20px" />
+                  เพิ่มข้อมูล
+                </button>
+              </div>
+            </div>
           </div>
 
           <div className="work-form-submit">
